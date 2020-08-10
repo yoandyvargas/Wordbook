1e906dfc0e4af88dd0661b7534036b88cd43155a
yoandyvargas <46614370+yoandyvargas@users.noreply.github.com>
2020-08-10 15:22:50 -0400

    Updates

---
+++ precache-manifest.30636683ee6d2285832384ea8153f8f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae7745b562217b46a0a285770598805",
+    "url": "/Wordbook/index.html"
+  },
+  {
+    "revision": "9764e0cce647fbbb24cf",
+    "url": "/Wordbook/static/css/main.0a82fc5a.chunk.css"
+  },
+  {
+    "revision": "523a812eb9f1d6e6f19d",
+    "url": "/Wordbook/static/js/2.b21ca04b.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/Wordbook/static/js/2.b21ca04b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9764e0cce647fbbb24cf",
+    "url": "/Wordbook/static/js/main.75dc2d84.chunk.js"
+  },
+  {
+    "revision": "85f05acd5c30d6f94489",
+    "url": "/Wordbook/static/js/runtime-main.74beac5d.js"
+  }
+]);
